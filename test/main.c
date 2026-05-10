c26870329f85268b3981904cb0e24b8903bb51dc
klongrich <longrichk@gmail.com>
2026-05-10 08:33:28 -0400

    updating test

--- test/main.c
+++ test/main.c
@@ -2,11 +2,12 @@
 
 int	main() {
 
-	printf("Hello World\n");
+	 printf("%s : another str: %s\n", "Hello World", "Hello World 2");
 	//ft_printf("%10s\n", "Hello World");
 	//ft_printf("%-#10.5hhx: after text %.5s again, %%, %-5.12X lakdsfj %#.12llo lklkj\n", "Hello World");
-	//ft_printf("%10h", NULL);
-	printf("%10h aldkfj\n", NULL);
-	ft_printf("%10h alsdk %10s\n", NULL);
+	//ft_printf("%s : another str: %s\n", "Hello World", "Hello World 2");
+	ft_printf("%hhd - %hd - %d - %hhd\n",  81029, 90384949399, 85930, 7849);
+	//printf("%hhd - %hd - %d - %ld - %lld\n",  81029, 90384949399, 2039884, 589938L, 7483038LL);
+	//ft_printf("%d - %ld adfdsa\n", 85040, 85949);
 	return (0);
 }
