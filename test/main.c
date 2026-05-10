e264e12675525d762f988fc34a7cface169d6c15
klongrich <longrichk@gmail.com>
2026-05-10 07:43:22 -0400

    adding test

--- test/main.c
+++ test/main.c
@@ -3,6 +3,10 @@
 int	main() {
 
 	printf("Hello World\n");
-	ft_printf("%10s\n", "Hello World");
+	//ft_printf("%10s\n", "Hello World");
+	//ft_printf("%-#10.5hhx: after text %.5s again, %%, %-5.12X lakdsfj %#.12llo lklkj\n", "Hello World");
+	//ft_printf("%10h", NULL);
+	printf("%10h aldkfj\n", NULL);
+	ft_printf("%10h alsdk %10s\n", NULL);
 	return (0);
 }
