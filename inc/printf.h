d02b0064eed0a2dc8abc8923435c157a89458b30
klongrich <longrichk@gmail.com>
2026-05-10 12:49:54 -0400

    adding tester

--- inc/printf.h
+++ inc/printf.h
@@ -2,6 +2,21 @@
 #include <unistd.h>
 #include "../libft/libft.h"
 
+#define CHECK(format, ...) do { \
+    int r1, r2; \
+    printf("checking %s\n", format); \
+    r1 = printf("ORIG: " format, __VA_ARGS__); \
+    printf("\n"); \
+    r2 = ft_printf("MINE: " format, __VA_ARGS__); \
+    printf("\n"); \
+    if (r1 != r2) { \
+        printf("\033[31m[KO] Return values differ! (Orig: %d, Mine: %d)\033[0m\n", r1, r2); \
+    } else { \
+        printf("\033[32m[OK] Return values match: %d\033[0m\n", r1); \
+    } \
+    printf("--------------------\n"); \
+} while (0)
+
 int    ft_printf(char *str, ...); 
 
 typedef struct args {
